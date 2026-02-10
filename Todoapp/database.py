d612d3e352d38add012f5a4ed73d0f383935eb15
Remya Roy <remyaroy023@gmail.com>
2026-02-10 11:18:35 +0530

    add sqlite database

--- Todoapp/database.py
+++ Todoapp/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:database1234@127.0.0.1:3306/todoapplicationdatabase'
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread": False})
 Sessionlocal = sessionmaker(autocommit = False,autoflush=False,bind=engine)
 Base = declarative_base()
